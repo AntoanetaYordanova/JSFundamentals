c166463f8691dfe8abcd7066197d905c8ab36713
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-07-18 12:07:54 +0200

    JS Exercises

--- Exams/04. Programming Fundamentals Final Exam/heroesOfCodeAndLogic.js
+++ Exams/04. Programming Fundamentals Final Exam/heroesOfCodeAndLogic.js
@@ -1,26 +1,108 @@
 function heroesOfCode(inputArr) {
-    let heroesNum = inputArr.shift();
-    let heroes = [];
+  let heroesNum = inputArr.shift();
+  let heroes = {};
 
-    for(let i = 0; i < heroesNum; i++) {
-        heroes.push(inputArr.shift());
+  for (let i = 0; i < heroesNum; i++) {
+      let currentHero = inputArr.shift();
+    let [heroName, hp, mp] = currentHero.split(" ");
+    hp = Number(hp);
+    mp = Number(mp);
+    heroes[heroName] = {
+      hp,
+      mp,
+    };
+  }
+
+  for (let command of inputArr) {
+    let actionArr = command.split(" - ");
+
+    if (actionArr[0] === "End") {
+      break;
+    } else if (actionArr[0] === "CastSpell") {
+      let heroName = actionArr[1];
+      let mpNeeded = Number(actionArr[2]);
+      let spellName = actionArr[3];
+
+      if (heroes[heroName].mp >= mpNeeded) {
+        heroes[heroName].mp -= mpNeeded;
+        console.log(
+          `${heroName} has successfully cast ${spellName} and now has ${heroes[heroName].mp} MP!`
+        );
+      } else {
+        console.log(
+          `${heroName} does not have enough MP to cast ${spellName}!`
+        );
+      }
+    } else if (actionArr[0] === "TakeDamage") {
+      let heroName = actionArr[1];
+      let damage = Number(actionArr[2]);
+      let attacker = actionArr[3];
+
+      if (heroes[heroName].hp > damage) {
+        heroes[heroName].hp -= damage;
+        console.log(
+          `${heroName} was hit for ${damage} HP by ${attacker} and now has ${heroes[heroName].hp} HP left!`
+        );
+      } else {
+        delete heroes[heroName];
+        console.log(`${heroName} has been killed by ${attacker}!`);
+      }
+    } else if (actionArr[0] === "Recharge") {
+      let heroName = actionArr[1];
+      let recharge = Number(actionArr[2]);
+      let newValue = recharge + heroes[heroName].mp;
+
+      if (newValue >= 200) {
+        let difference = newValue - 200;
+        recharge = recharge - difference;
+        heroes[heroName].mp = 200;
+        console.log(`${heroName} recharged for ${recharge} MP!`);
+      } else {
+        heroes[heroName].mp = newValue;
+        console.log(`${heroName} recharged for ${recharge} MP!`);
+      }
+    } else if (actionArr[0] === "Heal") {
+      let heroName = actionArr[1];
+      let heal = Number(actionArr[2]);
+      let newValue = heal + heroes[heroName].hp;
+
+      if (newValue >= 100) {
+        let difference = newValue - 100;
+        heal = heal - difference;
+        heroes[heroName].hp = 100;
+        console.log(`${heroName} healed for ${heal} HP!`);
+      } else {
+        heroes[heroName].hp = newValue;
+        console.log(`${heroName} healed for ${heal} HP!`);
+      }
     }
+  }
 
-    for(let command of inputArr) {
-        let actionArr = command.split(' - ');
+  let hpObj = {};
 
-        if(actionArr[0] === 'End') {
-            break;
-        } else if (actionArr[0] === 'CastSpell') {
-            
-        } else if (actionArr[0] === 'TakeDamage') {
+  let heroesNames = Object.keys(heroes);
 
-        } else if (actionArr[0] === 'Recharge') {
+  for(let heroName of heroesNames) {
+    hpObj[heroName] = heroes[heroName].hp;
+  }
 
-        } else if (actionArr[0] === 'Heal') {
+  let sortedHeroes = Object.entries(hpObj) .sort((a,b) => b[1] - a[1] || a[0].localeCompare(b[0]));
+  
 
-        }
-    }
+  for(let [heroName, hp] of sortedHeroes) {
+    console.log(heroName);
+    console.log(`  HP: ${heroes[heroName].hp}`);
+    console.log(`  MP: ${heroes[heroName].mp}`);
+  }
 }
 
-heroesOfCode([ '2', 'Solmyr 85 120', 'Kyrre 99 50', 'Heal - Solmyr - 10', 'Recharge - Solmyr - 50', 'TakeDamage - Kyrre - 66 - Orc', 'CastSpell - Kyrre - 15 - ViewEarth', 'End' ])
+heroesOfCode([
+  "2",
+  "Solmyr 85 120",
+  "Kyrre 99 50",
+  "Heal - Solmyr - 10",
+  "Recharge - Solmyr - 50",
+  "TakeDamage - Kyrre - 66 - Orc",
+  "CastSpell - Kyrre - 15 - ViewEarth",
+  "End",
+]);
--- Exams/04. Programming Fundamentals Final Exam/passwordReset.js
+++ Exams/04. Programming Fundamentals Final Exam/passwordReset.js
@@ -1,5 +1,50 @@
-function passwordReset(params) {
-    
+function passwordReset(inputArr) {
+  let str = inputArr.shift();
+  let newPass = "";
+
+  for (let command of inputArr) {
+    if(command === 'Done') {
+        break;
+    }
+
+    command = command.split(" ");
+
+    if(command[0] === 'TakeOdd') {
+        for (let i = 1; i < str.length; i += 2) {
+            newPass += str[i];
+          }
+          console.log(newPass);
+    } else if (command[0] === 'Cut') {
+        let index = Number(command[1]);
+        let length = Number(command[2]);
+        let substring = newPass.substring(index, index + length);
+        newPass = newPass.replace(substring, '');
+        console.log(newPass);
+    } else if (command[0] === 'Substitute') {
+        let oldSubstr = command[1];
+        let newSubstr = command[2];
+
+        if(newPass.includes(oldSubstr)) {
+
+            while(newPass.includes(oldSubstr)) {
+                newPass = newPass.replace(oldSubstr, newSubstr);
+            }
+
+            console.log(newPass);
+        } else {
+            console.log("Nothing to replace!");
+        }    
+    }
+  }
+  console.log(`Your password is: ${newPass}`);
+  
 }
 
-passwordReset()
+passwordReset([
+  "Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr ",
+  "TakeOdd",
+  "Cut 15 3",
+  "Substitute :: -",
+  "Substitute | ^",
+  "Done",
+]);
