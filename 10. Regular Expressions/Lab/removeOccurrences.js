64218b4895bb114b34db4433c36466028eedf755
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-07-24 08:14:31 +0200

    JS Exercises

--- 10. Regular Expressions/Lab/removeOccurrences.js
+++ 10. Regular Expressions/Lab/removeOccurrences.js
@@ -1,5 +1,9 @@
 function removeOccurrences(remove, str) {
-    
+    while(str.includes(remove)) {
+      str = str.replace(remove, '')
+    }
+
+    console.log(str);
 }
 
-removeOccurrences()
+removeOccurrences('ice', 'kicegiciceeb')
